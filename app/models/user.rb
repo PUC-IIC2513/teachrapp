3a6e90d532014f83aea3914729ca276797b86026
Raúl Montes <raulmt@gmail.com>
2014-08-25 21:43:08 -0400

    Algunas validaciones iniciales a User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true, length: { in: 2..50 }
+  validates :last_name, presence: true, length: { in: 2..50 }
+  validates :email, presence: true, format: /\A[a-z0-9]+[a-z0-9\._-]*@[a-z0-9\.]+\.[a-z]{2,5}\z/i, uniqueness: true
 end
