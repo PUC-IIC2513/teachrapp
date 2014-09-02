f16b939915a5bc2de454104167a410ccc51407f0
Raúl Montes <raulmt@gmail.com>
2014-09-01 22:54:16 -0400

    Nombre completo para User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true, length: { in: 2..50 }
   validates :last_name, presence: true, length: { in: 2..50 }
   validates :email, presence: true, format: /\A[a-z0-9]+[a-z0-9\._-]*@[a-z0-9\.]+\.[a-z]{2,5}\z/i, uniqueness: true
+
+  def complete_name
+    [first_name, last_name].join(' ')
+  end
+
 end
