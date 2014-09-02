ca330ff88ea09e2336ca3cd483e35657a5b16625
Raúl Montes <raulmt@gmail.com>
2014-09-01 22:50:37 -0400

    Seeds para cargar datos iniciales

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = (1..50).map do |i|
+  User.create(
+    first_name: "Raúl #{i}",
+    last_name: "Devés #{i}",
+    email: "raul#{i}@ing.puc.cl",
+    password: 'dalomismo',
+    role: 'profesor'
+  )
+end
+
+tags = (1..20).map do |i|
+  Tag.create(
+    name: "tag #{i}"
+  )
+end
+
+resources = (1..20).map do |i|
+  Resource.create(
+    name: "clase #{i}",
+    tags: tags.sample(5),
+    user: users.sample(1).first
+  )
+end
+
+announcements = (1..20).map do |i|
+  Announcement.create(
+    title: "Anuncio #{i}",
+    content: "Lorem Ipsum #{i}",
+    user: users.sample(1).first
+  )
+end
