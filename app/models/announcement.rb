6f798d32d3a325a75258a69f120cd1e37411b6ad
Raúl Montes <raulmt@gmail.com>
2014-09-01 22:50:24 -0400

    Arreglo de nombre de opción de validador

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,6 +1,6 @@
 class Announcement < ActiveRecord::Base
   belongs_to :user
 
-  validates :title, presence: true, length: { min: 5 }
+  validates :title, presence: true, length: { minimum: 5 }
   validates :level, numericality: { only_integer: true, greater_than: 1, less_than: 3 }
 end
