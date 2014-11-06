4bab9a3c1ce88c03669a152a0a99665ab353a9d6
Raúl Montes <raulmt@gmail.com>
2014-11-06 00:01:36 -0300

    Actualizado seeds con proyectos y groupos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,86 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = (1..50).map do |i|
-  User.create(
-    first_name: "Raúl #{i}",
-    last_name: "Devés #{i}",
-    email: "raul#{i}@ing.puc.cl",
+projects = Project.create([
+  { name: 'Librería de videojuegos', description: 'TODO' },
+  { name: 'IoU', description: 'TODO' },
+  { name: 'Bounty Hunters', description: 'TODO' }
+])
+
+repos = ['google/material-design-icons', 'davidsonfellipe/awesome-wpo', 'segmentio/nightmare', 'getify/You-Dont-Know-JS', 'facebook/osquery',
+  'facebook/AsyncDisplayKit', 'kimmobrunfeldt/progressbar.js', 'ConnorAtherton/walkway', 'enaqx/awesome-react', 'SFTtech/openage', 'keen/dashboards',
+  'aFarkas/lazysizes', 'sosedoff/pgweb', 'angular/angular.js', 'twbs/bootstrap', 'FezVrasta/bootstrap-material-design', 'fastos/fastsocket', 'google/physical-web', 'atom/atom',
+  'vhf/free-programming-books', 'jonschlinkert/remarkable', 'meteor/meteor', 'github/fetch', 'haoel/leetcode', 'facebook/react']
+
+count = 0
+groups = repos.map do |repo|
+  project = projects.sample(1).first
+  group = Group.create(name: "Grupo #{count += 1}", project: project, git_repo: repo)
+end
+
+names = [
+  'Zana Zinck',
+  'Alfredia Annunziata',
+  'Tiffiny To',
+  'Margert Marty',
+  'Tandra Theriault',
+  'Numbers Nishimura',
+  'Delphia David',
+  'Sunshine Sicard',
+  'Donnell Denbow',
+  'Richard Rock',
+  'Julissa Jobst',
+  'Georgina Gilbreath',
+  'Caridad Champlin',
+  'Violeta Villacis',
+  'Philip Pinkard',
+  'Elvera Esh',
+  'Pearlie Pellegrino',
+  'Alla Addis',
+  'Pamila Peart',
+  'Yun Younger',
+  'Cedric Cowart',
+  'Kary Klassen',
+  'Hoyt Hegland',
+  'Liza Lamay',
+  'Kizzy Klock',
+  'Olevia Olive',
+  'Isaac Isham',
+  'Will Wetherington',
+  'Jin Juliano',
+  'Eun Ewers',
+  'Ellan Eccles',
+  'Magaret Moncayo',
+  'Doris Dennen',
+  'Scarlet Stoehr',
+  'Lincoln Levario',
+  'Joye Junker',
+  'Theron Tibbits',
+  'Katlyn Krok',
+  'Beth Boutte',
+  'Micheline Miler',
+  'Annalee Adolphson',
+  'Milagros Mcnitt',
+  'Mitchel Mccarthy',
+  'Carissa Carmouche',
+  'Olga Oyola',
+  'Melonie Maldanado',
+  'Lizette Lightcap',
+  'Simonne Schuett',
+  'Francis Francoeur',
+  'Sharyn Seavey'
+]
+
+group_count = -1
+users = names.map do |name|
+  first_name, last_name = name.split(' ')
+  User.create!(
+    first_name: first_name,
+    last_name: last_name,
+    email: "#{name.gsub(' ', '.')}@example.org".downcase,
     password: 'dalomismo',
-    role: 'profesor'
+    role: 'student',
+    group: groups[(group_count += 1) / 2]
   )
 end
 
